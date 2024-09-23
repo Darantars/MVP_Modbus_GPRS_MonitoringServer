1eea72ac808b9930dd28e8b4d347c8a99714185e
Darantar <nikita.bugayev@mechatronica-pro.com>
2024-09-23 17:40:38 +0700

    Затычка для авторизации MVadmin : NotNSO

---
+++ Autorization/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Read_Write_GPRS_Server.Autorization
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
