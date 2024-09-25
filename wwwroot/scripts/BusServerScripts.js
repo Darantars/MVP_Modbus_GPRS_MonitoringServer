3077a3d32486b00ae78dd61e58824d61eb45f5b6
Darantar <nikita.bugayev@mechatronica-pro.com>
2024-09-25 10:25:54 +0700

    Нормальное разделение для вьюхи клиента. Для LiftView не нужно так как его вскоре все равно выпиливать и перекладывать в ф-л таблиц или перерабатывать

---
+++ wwwroot/scripts/BusServerScripts.js
@@ -0,0 +1,104 @@
+﻿async function Home() {
+    await fetch('/api/TCP/stop');
+    window.location.href = '/Home';
+}
+
+async function StartConnection() {
+    const connectionPort = document.getElementById('connectionPort').value;
+    await fetch(`/api/TCP/start?connectionPort=${connectionPort}`);
+}
+
+async function StopConnection() {
+    await fetch('/api/TCP/stop');
+}
+
+async function updateData() {
+    const response = await fetch('/api/TCP/read');
+    const data = await response.text();
+    const newData = `<div>${data}</div>`;
+    document.getElementById('data').innerHTML = newData;
+
+    const response1 = await fetch('/api/TCP/updateConnectionStatus');
+    const connection = await response1.text();
+    const newStatus = `${connection}`;
+    document.getElementById('connectionStatus').innerHTML = newStatus;
+}
+
+async function SendToDevice() {
+    const message = document.getElementById('messageInput').value;
+    const response = await fetch('/api/TCP/send', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: message
+    });
+
+    if (response.ok) {
+        alert('Message sent successfully!');
+    } else {
+        const errorText = await response.text();
+        alert('Failed to send message: ' + errorText);
+    }
+}
+
+async function SendMb3ReadToDevice() {
+    const modbusID = document.getElementById('modbusID').value;
+    const modbusStartAdress = document.getElementById('modbusStartAdress').value;
+    const modbusInput =
+    {
+        modbusID: modbusID,
+        modbusStartAdress: modbusStartAdress
+    };
+    const response = await fetch('/api/TCP/sendMb3',
+        {
+            method: 'POST',
+            headers:
+            {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(modbusInput)
+        });
+    if (response.ok) {
+        alert('Modbus command sent successfully!');
+    }
+    else {
+        const errorText = await response.text();
+        alert('Failed to send Modbus command: ' + errorText);
+    }
+}
+
+async function SendMb10WriteToDevice() {
+    const modbusID = document.getElementById('modbusID').value;
+    const modbusStartAdress = document.getElementById('modbusStartAdress').value;
+    const modbussQuanity = document.getElementById('modbussQuanity').value;
+    const modbussData = document.getElementById('modbussData').value;
+
+    const modbusInput =
+    {
+        modbusID: modbusID,
+        modbusStartAdress: modbusStartAdress,
+        modbussQuanity: modbussQuanity,
+        modbussData: modbussData
+    };
+    const response = await fetch('/api/TCP/sendMb10',
+        {
+            method: 'POST',
+            headers:
+            {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(modbusInput)
+        });
+    if (response.ok) {
+        alert('Modbus command sent successfully!');
+    }
+    else {
+        const errorText = await response.text();
+        alert('Failed to send Modbus command: ' + errorText);
+    }
+}
+
+
+setInterval(updateData, 200); // Обновление каждые 200 миллисекунд
+updateData(); // Первоначальное обновление
---
+++ wwwroot/scripts/TableScripts.js
@@ -0,0 +1,347 @@
+﻿document.addEventListener('DOMContentLoaded', async function () {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+
+    await UploadSavedTables(); // Загрузка сохраненных таблиц при загрузке страницы
+
+    setInterval(updateData, 100); // Обновление каждые 100 миллисекунд
+    updateData(); // Первоначальное обновление
+});
+
+let tables = [];
+let updateToken = true;
+
+async function Home() {
+    StopConnection();
+    window.location.href = '/Home';
+}
+async function StartConnection() {
+    const connectionPort = document.getElementById('connectionPort').value;
+    await fetch(`/api/Table/start?connectionPort=${connectionPort}`);
+}
+async function StopConnection() {
+    await fetch('/api/Table/stop');
+}
+
+async function addTable() {
+    const tableId = document.getElementById('newTableId').value;
+    const names = document.getElementById('newTableNames').value;
+    const addresses = document.getElementById('addresses').value;
+    const sizes = document.getElementById('newTableSizes').value;
+    const types = document.getElementById('newTableTypes').value;
+    const unitTypes = document.getElementById('newTableUnitTypes').value;
+    const formats = document.getElementById('newTableFormats').value;
+
+    if (!tableId || !addresses) {
+        alert('Пожалуйста, заполните все поля.');
+        return;
+    }
+
+    const namesArray = names.split(',').map(String);
+    const addressesArray = addresses.split(',').map(Number);
+    const sizesArray = sizes.split(',').map(Number);
+    const typesArray = types.split(',').map(String);
+    const unitTypesArray = unitTypes.split(',').map(String);
+    const formatsArray = formats.split(',').map(String);
+
+    const response = await fetch(`/api/Table/AddNewTable`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: tableId, names: namesArray, addresses: addressesArray, sizes: sizesArray, types: typesArray, unitTypes: unitTypesArray, formats: formatsArray })
+    });
+
+    if (response.ok) {
+        const tableContainer = document.createElement('div');
+        tableContainer.innerHTML =
+            `
+                    <section class="table-section">
+                        <h4 class="table-section-header">${tableId}</h4>
+                        <div class="table-section-body">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Название параметра</th>
+                                        <th>Значение</th>
+                                        <th>Ед.изм</th>
+                                        <th>Адрес</th>
+                                        <th>Формат</th>
+                                        <th>Вид</th>
+                                        <th>Размер</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="${tableId}">
+                                    ${namesArray.map((name, index) => `
+                                        <tr>
+                                            <td>${name}</td>
+                                            <td>Не опрашивается</td>
+                                            <td>${unitTypesArray[index]}</td>
+                                            <td>${addressesArray[index]}</td>
+                                            <td>${formatsArray[index]}</td>
+                                            <td>${typesArray[index]}</td>
+                                            <td>${sizesArray[index]}</td>
+                                        </tr>
+                                    `).join('')}
+                                </tbody>
+                            </table>
+
+                            <div class="table-section-log-control">
+                                <span class="log-status">Лог отключен</span>
+                                <button class="btn waves-effect waves-light blue">Логировать</button>
+                                <button class="btn waves-effect waves-light blue">Выгрузить лог</button>
+                            </div>
+                        </div>
+                    </section>
+                `;
+        document.getElementById('tablesContainer').appendChild(tableContainer);
+        tables.push({ id: tableId, names: namesArray, addresses: addressesArray, sizes: sizesArray, types: typesArray, unitTypes: unitTypesArray, formats: formatsArray });
+    }
+    else {
+        alert('Ошибка при добавлении таблицы.');
+    }
+}
+
+async function updateData() {
+    const response = await fetch(`/api/Table/GetConnectionStatus`);
+    const connectionStatus = await response.text();
+    document.getElementById(`conectionStatus`).innerHTML = connectionStatus;
+
+    if (updateToken === true) {
+        updateToken = false;
+
+        // *** Работа с таблицей ***
+        const modbusID = document.getElementById('modbusID').value;
+        if (modbusID != "") {
+            for (const table of tables) {
+                const tableId = table.id;
+                const tableDataResponse = await fetch(`/api/Table/GetTableData?modbusID=${modbusID}&tableId=${tableId}`);
+                const tableData = await tableDataResponse.json();
+
+                const tableBody = document.getElementById(tableId);
+
+                if (tableData.includes("Не опрашивается")) {
+                    // Заполнение таблицы строками с "Не опрашивается"
+                    table.names.forEach((name, index) => {
+                        const tr = tableBody.querySelectorAll('tr')[index];
+
+                        // Добавление столбца с названиями параметров
+                        const tdValue = tr.querySelectorAll('td')[1];
+                        tdValue.textContent = "Не опрашивается";
+                    });
+                } else {
+                    // Заполнение таблицы реальными данными
+                    tableData.forEach((row, index) => {
+                        const tr = tableBody.querySelectorAll('tr')[index];
+
+                        // Добавление столбца с названиями параметров
+                        const tdValue = tr.querySelectorAll('td')[1];
+                        tdValue.textContent = row;
+                    });
+                }
+            }
+        }
+
+        updateToken = true;
+    }
+}
+
+function handleFileUpload(event) {
+
+    /*********                               ********* KEIL *********                                *********/
+
+    const file = event.target.files[0];
+    const fileName = file.name.replace(/\.[^/.]+$/, ""); // Удаляем расширение файла
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+        // Пропускаем первую строку (заголовок)
+        const dataRows = jsonData.slice(1);
+
+        let currentTableId = null;
+        let currentTable = null;
+        let currentGroupName = null;
+
+        dataRows.forEach(row => {
+            const idParts = row[0].split('.');
+            const tableIdPart = idParts[0];
+            const groupName = row[1];
+            const parameter = idParts[2];
+
+            const tableId = `${fileName}    ${groupName}`;
+
+            if (tableIdPart !== currentTableId) {
+                if (currentTable) {
+                    addTableFromData(`${fileName}   ${currentGroupName}`, currentTable.names, currentTable.addresses, currentTable.sizes, currentTable.types, currentTable.unitTypes, currentTable.formats);
+                }
+                currentTableId = tableIdPart;
+                currentGroupName = groupName;
+                currentTable = {
+                    names: [],
+                    addresses: [],
+                    sizes: [],
+                    types: [],
+                    unitTypes: [],
+                    formats: []
+                };
+            }
+
+            currentTable.names.push(row[1]);
+            currentTable.unitTypes.push(row[3]);
+            currentTable.types.push(row[5]);
+            currentTable.formats.push(row[4]);
+            currentTable.addresses.push(row[6]);
+            currentTable.sizes.push(row[12]);
+        });
+
+        if (currentTable) {
+            addTableFromData(`${fileName}   ${currentGroupName}`, currentTable.names, currentTable.addresses, currentTable.sizes, currentTable.types, currentTable.unitTypes, currentTable.formats);
+        }
+    };
+
+    reader.readAsArrayBuffer(file);
+}
+
+async function addTableFromData(tableId, names, addresses, sizes, types, unitTypes, formats) {
+    try {
+        // Преобразование строк в числа
+        addresses = addresses.map(addr => addr === "" ? 0 : parseInt(addr, 10));
+        sizes = sizes.map(size => size === "" ? 0 : parseInt(size, 10));
+
+        const jsonData = JSON.stringify({
+            id: tableId,
+            names: names,
+            addresses: addresses,
+            sizes: sizes,
+            types: types,
+            unitTypes: unitTypes,
+            formats: formats
+        });
+        console.log('JSON данные для отправки:', jsonData);
+        const response = await fetch(`/api/Table/AddNewTable`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: tableId,
+                names: names,
+                addresses: addresses,
+                sizes: sizes,
+                types: types,
+                unitTypes: unitTypes,
+                formats: formats
+            })
+        });
+
+        if (response.ok) {
+            const tableContainer = document.createElement('div');
+            tableContainer.innerHTML =
+                `
+                        <section class="table-section">
+                            <h4 class="table-section-header">${tableId}</h4>
+                            <div class="table-section-body">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Название параметра</th>
+                                            <th>Значение</th>
+                                            <th>Ед.изм</th>
+                                            <th>Адрес</th>
+                                            <th>Формат</th>
+                                            <th>Вид</th>
+                                            <th>Размер</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="${tableId}">
+                                        ${names.map((name, index) => `
+                                            <tr>
+                                                <td>${name}</td>
+                                                <td>Не опрашивается</td>
+                                                <td>${unitTypes[index]}</td>
+                                                <td>${addresses[index]}</td>
+                                                <td>${formats[index]}</td>
+                                                <td>${types[index]}</td>
+                                                <td>${sizes[index]}</td>
+                                            </tr>
+                                        `).join('')}
+                                    </tbody>
+                                </table>
+
+                                <div class="table-section-log-control">
+                                    <span class="log-status">Лог отключен</span>
+                                    <button class="btn waves-effect waves-light blue">Логировать</button>
+                                    <button class="btn waves-effect waves-light blue">Выгрузить лог</button>
+                                </div>
+                            </div>
+                        </section>
+                    `;
+            document.getElementById('tablesContainer').appendChild(tableContainer);
+            tables.push({ id: tableId, names: names, addresses: addresses, sizes: sizes, types: types, unitTypes: unitTypes, formats: formats });
+        } else {
+            alert('Ошибка при добавлении таблицы.');
+        }
+    } catch (error) {
+        console.error('Ошибка при отправке данных на сервер:', error);
+        alert('Произошла ошибка при отправке данных на сервер. Пожалуйста, проверьте формат данных.');
+    }
+}
+
+async function UploadSavedTables() {
+    const response = await fetch('/api/Table/GetSavedTables');
+    const tablesData = await response.json();
+
+    tablesData.forEach(tableData => {
+        const { id, names, addresses, sizes, types, unitTypes, formats } = tableData;
+
+        const tableContainer = document.createElement('div');
+        tableContainer.innerHTML =
+            `
+                <section class="table-section">
+                    <h4 class="table-section-header">${id}</h4>
+                    <div class="table-section-body">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Название параметра</th>
+                                    <th>Значение</th>
+                                    <th>Ед.изм</th>
+                                    <th>Адрес</th>
+                                    <th>Формат</th>
+                                    <th>Вид</th>
+                                    <th>Размер</th>
+                                </tr>
+                            </thead>
+                            <tbody id="${id}">
+                                ${names.map((name, index) => `
+                                    <tr>
+                                        <td>${name}</td>
+                                        <td>Не опрашивается</td>
+                                        <td>${unitTypes[index]}</td>
+                                        <td>${addresses[index]}</td>
+                                        <td>${formats[index]}</td>
+                                        <td>${types[index]}</td>
+                                        <td>${sizes[index]}</td>
+                                    </tr>
+                                `).join('')}
+                            </tbody>
+                        </table>
+
+                        <div class="table-section-log-control">
+                            <span class="log-status">Лог отключен</span>
+                            <button class="btn waves-effect waves-light blue">Логировать</button>
+                            <button class="btn waves-effect waves-light blue">Выгрузить лог</button>
+                        </div>
+                    </div>
+                </section>
+            `;
+        document.getElementById('tablesContainer').appendChild(tableContainer);
+        tables.push({ id, names, addresses, sizes, types, unitTypes, formats });
+    });
+}
