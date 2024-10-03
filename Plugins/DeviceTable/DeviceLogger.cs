bfe6789d1d54207b4aeb2863ddc47c4659558f02
Darantar <nikita.bugayev@mechatronica-pro.com>
2024-10-03 17:56:10 +0700

    Допилен класс логгера

--- Plugins/DeviceTable/DeviceLogger.cs
+++ Plugins/DeviceTable/DeviceLogger.cs
@@ -1,53 +1,70 @@
-﻿using System.Data;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Read_Write_GPRS_Server.Plugins.DeviceTable
 {
     public class DeviceLogger
     {
-        public string deviceId { get; set; }
+        public string DeviceId { get; set; }
 
-        public string deviceLog { get; set; }
+        private string deviceLog { get; set; }
 
         private DateTime lastFileCreateTime { get; set; }
 
         private string logFilePath { get; set; }
 
-        private string lofDirectoryPath { get; set; }
-
-
+        private string logDirectoryPath { get; set; }
 
         public DeviceLogger(string newDeviceId)
         {
-            deviceId = newDeviceId;
-
+            DeviceId = newDeviceId;
             deviceLog = "";
+            logDirectoryPath = Path.GetFullPath(@"UserData\Logs");
+            lastFileCreateTime = DateTime.MinValue;
 
-            lofDirectoryPath =  @"..\..\UserData\Logs";
+            Console.WriteLine($"Log directory path: {logDirectoryPath}");
         }
 
-        public void LogParamChange(DateTime changeTime, string parametrName, string value)
+        public async Task LogParamChangeAsync(DateTime changeTime, string parametrName, string value)
         {
             if (changeTime > lastFileCreateTime + TimeSpan.FromHours(3))
-                CreateNewLogFile();
+            {
+                await CreateNewLogFileAsync();
+            }
             deviceLog += $"{changeTime.ToString("dd.MM.yyyy HH:mm:ss")} - {parametrName}: {value}\r\n";
+            await WriteLogToFileAsync();
         }
 
-        private void CreateNewLogFile() 
+        private async Task CreateNewLogFileAsync()
         {
-            if (!Directory.Exists(lofDirectoryPath + $"\\{deviceId}"))
+            string deviceDirectoryPath = Path.Combine(logDirectoryPath, DeviceId);
+            if (!Directory.Exists(deviceDirectoryPath))
             {
-                Directory.CreateDirectory(lofDirectoryPath + $"\\{deviceId}");
+                Directory.CreateDirectory(deviceDirectoryPath);
+                Console.WriteLine($"Created device directory: {deviceDirectoryPath}");
             }
+
+            string fileName = $"{DateTime.Now:yyyy.MM.dd.HH.mm.ss}.log";
+            logFilePath = Path.Combine(deviceDirectoryPath, fileName);
+            lastFileCreateTime = DateTime.Now;
+
+            Console.WriteLine($"Created new log file: {logFilePath}");
+        }
+
+        private async Task WriteLogToFileAsync()
+        {
             try
             {
-                File.Create(lofDirectoryPath  + $"\\{deviceId}" + DateTime.Now.ToString("yyyyMMddHHmmss"));
+                await File.WriteAllTextAsync(logFilePath, deviceLog, System.Text.Encoding.UTF8);
+                Console.WriteLine($"Log written to file: {logFilePath}");
+                deviceLog = "";
             }
-            catch 
+            catch (Exception ex)
             {
-                throw new Exception("Ошибка создания файла лога");
+                Console.WriteLine($"Error writing to log file: {ex.Message}");
+                throw new Exception("Ошибка записи в файл лога: " + ex.Message);
             }
-            logFilePath = lofDirectoryPath + $"\\{deviceId}" + DateTime.Now.ToString("yyyyMMddHHmmss");
         }
-
     }
 }
